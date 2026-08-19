521e0edf8408c3e05161694840ce53705d43c265
wanwuzhili <129184813+wanwuzhili@users.noreply.github.com>
2026-08-19 09:30:47 +0800

    3

--- exam.py
+++ exam.py
@@ -9,6 +9,7 @@ with open('./config/config.yaml', 'r') as f:
 
 train_iter, valid_iter = ds.get_data_loader(
     raw_data_path=configs['raw_data_path'], batch_size=configs['batch_size'],
+    num_workers=4, need_label_trans=True
     )
 
 for img, label in train_iter:
--- src/dataset.py
+++ src/dataset.py
@@ -1,54 +1,54 @@
+import os
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 
 from torchvision import io
 from sklearn.preprocessing import LabelEncoder
 import joblib
 
-def preprocess(raw_data_path, is_train=True, trans=None):
-    if is_train:
-        raw_data = pd.read_csv(raw_data_path+'train.csv')
-    else:
-        raw_data = pd.read_csv(raw_data_path+'test.csv')
-    images = raw_data.image
-    if trans:
-        features = [trans(io.read_image(raw_data_path+image))
-                     for image in images]
-    else:
-        features = [io.read_image(raw_data_path+image)
-                    for image in images]
-    features = torch.stack(features).type(torch.float32)
 
-    if is_train:
-        labels = raw_data.label
-        le = LabelEncoder()
-        labels = le.fit_transform(labels)
-        labels = torch.tensor(labels, dtype=torch.long)
-        joblib.dump(le, './le.pkl')
-        return features, labels
-    else:
-        return features
-
-
-class LeaveDataset(Dataset):
-    def __init__(self, features, labels):
+class ImageDataset(Dataset):
+    def __init__(self, img_file, train_csv_path, trans=None, label_trans:LabelEncoder=None):
         super().__init__()
-        self.f = features
-        self.label = labels
+        self.img_file = img_file
+        self.train_data = pd.read_csv(train_csv_path)
+        self.trans = trans
+        self.label_trans = label_trans
 
     def __len__(self):
-        return len(self.label)
+        return len(self.train_data)
 
-    def __getitem__(self, index):
-        return self.f[index], self.label[index]
+    def __getitem__(self, i):
+        img_path = os.path.join(self.img_file, self.train_data.iloc[i, 0])
+        img = io.read_image(img_path)
+        if self.trans:
+            img = self.trans(img)
+        label = self.train_data.iloc[i, 1]
+        if self.label_trans:
+            label = self.label_trans.transform([label])[0]
 
-def get_data_loader(raw_data_path, batch_size,num_workers=0, trans=None):
-    features, labels = preprocess(raw_data_path,trans=trans)
-    num_train = round(0.8 * len(labels))
-    train_dataset = LeaveDataset(features[:num_train], labels[:num_train])
-    valid_dataset = LeaveDataset(features[num_train:], labels[num_train:])
+        return img.type(torch.float32), torch.tensor(label, dtype=torch.long)
+
+def label_encoder(train_csv_path):
+    labels = pd.read_csv(train_csv_path).iloc[:, 1]
+    le = LabelEncoder()
+    le = le.fit(labels)
+    joblib.dump(le, './le.pkl')
+    return le
+
+def get_data_loader(img_file, train_csv_path, batch_size,
+                    num_workers=0, trans=None, need_label_trans=True):
+    if need_label_trans:
+        le = label_encoder(train_csv_path)
+    else:
+        le = None
+    dataset = ImageDataset(img_file, train_csv_path, trans=trans, label_trans=le)
+    num = dataset.__len__()
+    num_train = round(0.8 * num)
+    num_valid = num - num_train
+    train_dataset, valid_dataset = random_split(dataset, [num_train, num_valid])
     return (
         DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers),
-        DataLoader(valid_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+        DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     )
